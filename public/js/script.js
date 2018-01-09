6aa6347a8ddb38cdad233a1955e32c84c1cf47c3
DESKTOP-E7CEOVI\05 <webkmkm@gmail.com>
2018-01-09 09:59:13 +0900

    no message

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,23 @@
 $(function () {
+    $(".header-slide").slick({
+        autoplay: true,
+        autoplayspeed: 400,
+        fade: true,
+        pauseOnHover: false
+    });
+
+    $(".menu-trigger").stop().click(function () {
+        $(this).stop().toggleClass("active");
+        $(".gnav").stop().slideToggle();
+        return false;
+    });
+
+    $('.js-inview').on('inview', function (event, isInView, visiblePartX, visiblePartY) {
+        if (isInView) {
+            $(this).stop().addClass('anime');
+        } else {
+            $(this).stop().removeClass('anime');
+        }
+    });
 
 });
