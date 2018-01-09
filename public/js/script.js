a040cfec2661d2d9d0c32af8d0c840d3374925a7
DESKTOP-E7CEOVI\05 <webkmkm@gmail.com>
2018-01-09 16:04:03 +0900

    index完成

--- public/js/script.js
+++ public/js/script.js
@@ -1,16 +1,23 @@
 $(function () {
+
+    //slick-------------------------------------
     $(".header-slide").slick({
         autoplay: true,
-        autoplayspeed: 400,
+        autoplayspeed: 500,
         fade: true,
-        pauseOnHover: false
+        pauseOnHover: false,
+        arrows: false
     });
+
+    //ハンバーガーメニュー-------------------------
 
     $(".menu-trigger").stop().click(function () {
         $(this).stop().toggleClass("active");
-        $(".gnav").stop().slideToggle();
+        $(".gnav-320").stop().slideToggle();
         return false;
     });
+
+    //要素フレームインで表示-----------------------
 
     $('.js-inview').on('inview', function (event, isInView, visiblePartX, visiblePartY) {
         if (isInView) {
@@ -18,6 +25,31 @@ $(function () {
         } else {
             $(this).stop().removeClass('anime');
         }
+    });
+
+    $('.js-inview-slide').on('inview', function (event, isInView, visiblePartX, visiblePartY) {
+        if (isInView) {
+            $(this).stop().addClass('anime-header');
+        } else {
+            $(this).stop().removeClass('anime-header');
+        }
+    });
+
+    $('.js-inview-header').on('inview', function (event, isInView, visiblePartX, visiblePartY) {
+        if (isInView) {
+            $(this).stop().addClass('anime-header');
+        } else {
+            $(this).stop().removeClass('anime-header');
+        }
+    });
+
+    //フッターナブでホームに戻る--------------------
+
+    $(".gnav-footer-top").stop().click(function () {
+        $("html,body").stop().animate({
+            scrollTop: 0
+        }, 700);
+        return false;
     });
 
 });
