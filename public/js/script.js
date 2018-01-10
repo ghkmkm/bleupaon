8e59ef017e30913a0e0b26839a05eb228788eae8
DESKTOP-E7CEOVI\05 <webkmkm@gmail.com>
2018-01-10 17:40:52 +0900

    no message

--- public/js/script.js
+++ public/js/script.js
@@ -17,39 +17,49 @@ $(function () {
         return false;
     });
 
-    //要素フレームインで表示-----------------------
+    //要素フレームイン時のアクション-----------------
 
+    //メイン内画像ズーム
     $('.js-inview').on('inview', function (event, isInView, visiblePartX, visiblePartY) {
         if (isInView) {
-            $(this).stop().addClass('anime');
+            $(this).stop().addClass('show');
         } else {
-            $(this).stop().removeClass('anime');
+            $(this).stop().removeClass('show');
         }
     });
 
+    //トップページヘッダーフェードイン
     $('.js-inview-slide').on('inview', function (event, isInView, visiblePartX, visiblePartY) {
         if (isInView) {
-            $(this).stop().addClass('anime-header');
+            $(this).stop().addClass('show-fade');
         } else {
-            $(this).stop().removeClass('anime-header');
+            $(this).stop().removeClass('show-fade');
         }
     });
 
+    //ヘッダー内ナビゲーションフェードイン
     $('.js-inview-header').on('inview', function (event, isInView, visiblePartX, visiblePartY) {
         if (isInView) {
-            $(this).stop().addClass('anime-header');
+            $(this).stop().addClass('show-fade');
         } else {
-            $(this).stop().removeClass('anime-header');
+            $(this).stop().removeClass('show-fade');
         }
     });
 
-    //フッターナブでホームに戻る--------------------
+    $('.show-drop').on('inview', function (event, isInView, visiblePartX, visiblePartY) {
+        if (isInView) {
+            $(this).stop().addClass('show-drop2');
+        } else {
+            $(this).stop().removeClass('show-drop2');
+        }
+    });
 
+
+    //フッターナブでページトップに戻る--------------------
     $(".gnav-footer-top").stop().click(function () {
         $("html,body").stop().animate({
             scrollTop: 0
         }, 700);
         return false;
     });
-
 });
